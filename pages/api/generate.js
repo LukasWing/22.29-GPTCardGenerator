d3f086773a92e728546cbc60411977b4bb9b059f
Jakob Tolstrup <jakobtolstrup@Jakobs-MacBook-Pro.local>
2023-01-13 11:38:12 +0100

    added prompt and new logo

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -15,11 +15,11 @@ export default async function (req, res) {
     return;
   }
 
-  const animal = req.body.animal || '';
-  if (animal.trim().length === 0) {
+  const text = req.body.text || '';
+  if (text.trim().length === 0) {
     res.status(400).json({
       error: {
-        message: "Please enter a valid animal",
+        message: "Please enter valid text",
       }
     });
     return;
@@ -28,7 +28,7 @@ export default async function (req, res) {
   try {
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: generatePrompt(animal),
+      prompt: generatePrompt(text),
       temperature: 0.6,
     });
     res.status(200).json({ result: completion.data.choices[0].text });
@@ -48,15 +48,21 @@ export default async function (req, res) {
   }
 }
 
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
+function generatePrompt(text) {
+  const capitalizedQuestion =
+    text[0].toUpperCase() + text.slice(1).toLowerCase();
+  return `Generate a short question based on text..
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+Text1: The presentation layer is the user interface of a software application, responsible for displaying data and accepting user input.
+Question1: What is the presentation layer
+
+Text2: A memex is a device in which an individual stores all his books, records, and communications, and which is mechanized so that it may be consulted with exceeding speed and flexibility. It is an enlarged intimate supplement to his memory.
+Question2: What is a Memex
+
+Text3: "Spaced repetition systems work only as well as the prompts you give them. And especially when new to these systems, you’re likely to give them mostly bad prompts. It often won’t even be clear which prompts are bad and why, much less how to improve them."
+Question3: What is a key limitaiton of of spaced repitition systems as it relates to prompts
+
+Text: ${capitalizedQuestion}
+Question:`;
 }
+
